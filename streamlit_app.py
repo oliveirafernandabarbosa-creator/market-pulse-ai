f1ce0fe25cd20f12c108bfb82d2e13b8d4fb55bc
oliveirafernandabarbosa-creator <oliveirafernandabarbosa@gmail.com>
2026-09-17 14:47:00 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
-         import streamlit as st
+import streamlit as st
 import pandas as pd
 import altair as alt
 import os
-
 # ============================================================
 # CONFIGURAÇÃO
 # ============================================================
